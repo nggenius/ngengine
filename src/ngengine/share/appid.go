18af3deb4d92956a95177510373a5a19eadff2b1
mysll <liangliang.shi@gmail.com>
2018-01-28 15:08:49 +0800

    合并文件

--- src/ngengine/share/appid.go
+++
@@ -1,9 +0,0 @@
-package share
-
-type ServiceId uint16
-
-const (
-	SID_MAX        = 0xFFFF // service id max
-	MB_FLAG_APP    = iota   // app
-	MB_FLAG_CLIENT          // client
-)
--- src/ngengine/share/define.go
+++ src/ngengine/share/define.go
@@ -2,15 +2,25 @@ package share
 
 type SessionId uint64
 
+type ServiceId uint16
+
+const (
+	MAX_BUF_LEN = 1024 * 16      // 消息缓冲区大小16k
+	SID_MAX     = 0xFFFF         // service id 最大值
+	SESSION_MAX = 0x7FFFFFFFFFFF // session最大值
+)
+
+// mailbox类型定义
+const (
+	MB_FLAG_APP    = iota // app
+	MB_FLAG_CLIENT        // client
+)
+
 const (
 	ST_ADMIN = 1 + iota
 )
 
-const (
-	MAX_BUF_LEN = 1024 * 16      // 消息缓冲区大小16k
-	SESSION_MAX = 0x7FFFFFFFFFFF // session最大值
-)
-
+// 事件定义
 const (
 	EVENT_READY        = "ready"        // 别的服务就绪, args:{id:ServiceId}
 	EVENT_LOST         = "lost"         // 丢失服务, args:{id:ServiceId}
