37e5d3e03320ef4836cf85b322e44b83db79e1b3
mysll <liangliang.shi@gmail.com>
2018-02-02 22:25:09 +0800

    修改可访问性

---
+++ src/ngengine/module/object/module.go
@@ -0,0 +1,44 @@
+package object
+
+import "ngengine/core/service"
+
+type ObjectModule struct {
+	core           service.CoreApi
+	defaultFactory *factory // 默认对象工厂
+	factorys       map[string]*factory
+}
+
+func New() *ObjectModule {
+	o := &ObjectModule{}
+	o.defaultFactory = newFactory()
+	o.factorys = make(map[string]*factory)
+	return o
+}
+
+// Name 模块名
+func (o *ObjectModule) Name() string {
+	return "ObjectModule"
+}
+
+// Init 模块初始化
+func (o *ObjectModule) Init(core service.CoreApi) bool {
+	o.core = core
+	return true
+}
+
+// Shut 模块关闭
+func (o *ObjectModule) Shut() {
+
+}
+
+// OnUpdate 模块Update
+func (o *ObjectModule) OnUpdate(t *service.Time) {
+}
+
+// OnMessage 模块消息
+func (o *ObjectModule) OnMessage(id int, args ...interface{}) {
+}
+
+func (o *ObjectModule) RegisterObject(typ string, obj interface{}) {
+
+}
---
+++ src/ngengine/module/object/object.go
@@ -0,0 +1,4 @@
+package object
+
+type Object struct {
+}
