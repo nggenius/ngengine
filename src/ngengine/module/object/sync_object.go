cd986832071678be0c1cd1403c4c3f0334d802c1
Peterosh <908789002@qq.com>
2018-04-28 11:18:58 +0800

    Revert "Revert "增加镜像数据的上锁""
    This reverts commit 13ec1a426a3027815968179f30eb2fd6ba2c853b, reversing
    changes made to e6f6564b349a17bb614e089605b602072c3fd051.

--- src/ngengine/module/object/sync_object.go
+++ src/ngengine/module/object/sync_object.go
@@ -18,6 +18,10 @@ func (s *SyncObject) RegisterCallback(svr rpc.Servicer) {
 	svr.RegisterCallback("DelTableRow", s.DelTableRow)
 	svr.RegisterCallback("ClearTable", s.ClearTable)
 	svr.RegisterCallback("ChangeTable", s.ChangeTable)
+	svr.RegisterCallback("LockObj", s.LockObj)
+	svr.RegisterCallback("UnLockObj", s.UnLockObj)
+	svr.RegisterCallback("LockObjSuccess", s.LockObjSuccess)
+	svr.RegisterCallback("UnLockObjSuccess", s.UnLockObjSuccess)
 }
 
 // 对象属性变动
@@ -57,5 +61,63 @@ func (s *SyncObject) ClearTable(mailbox rpc.Mailbox, msg *protocol.Message) (err
 
 // 对象表格单元格更新
 func (s *SyncObject) ChangeTable(mailbox rpc.Mailbox, msg *protocol.Message) (errcode int32, reply *protocol.Message) {
+
+	return 0, nil
+}
+
+// LockObj 给对象上锁
+func (s *SyncObject) LockObj(mailbox rpc.Mailbox, msg *protocol.Message) (errcode int32, reply *protocol.Message) {
+	m := protocol.NewMessageReader(reply)
+	r, _ := m.ReadUInt32()
+	o, err := s.owner.FindObject(mailbox)
+	if err != nil {
+		s.owner.core.LogErr(err)
+	}
+
+	if obj, ok := o.(Object); ok {
+		obj.AddLocker(mailbox, r, true)
+	}
+	return 0, nil
+}
+
+// UnLockObj 给对象解锁
+func (s *SyncObject) UnLockObj(mailbox rpc.Mailbox, msg *protocol.Message) (errcode int32, reply *protocol.Message) {
+	m := protocol.NewMessageReader(reply)
+	r, _ := m.ReadUInt32()
+	o, err := s.owner.FindObject(mailbox)
+	if err != nil {
+		s.owner.core.LogErr(err)
+	}
+	if obj, ok := o.(Object); ok {
+		obj.UnLockObj(mailbox, r, true)
+	}
+	return 0, nil
+}
+
+// 远程通知对象上锁成功
+func (s *SyncObject) LockObjSuccess(mailbox rpc.Mailbox, msg *protocol.Message) (errcode int32, reply *protocol.Message) {
+	m := protocol.NewMessageReader(reply)
+	r, _ := m.ReadUInt32()
+	o, err := s.owner.FindObject(mailbox)
+	if err != nil {
+		s.owner.core.LogErr(err)
+	}
+
+	if obj, ok := o.(Object); ok {
+		// 这里远程回复上锁成功所以上锁的就是本对象切这里算上本地的锁
+		obj.LockObjSuccess(*obj.Original(), r, false)
+	}
+	return 0, nil
+}
+
+// 远程通知对象解锁成功
+func (s *SyncObject) UnLockObjSuccess(mailbox rpc.Mailbox, msg *protocol.Message) (errcode int32, reply *protocol.Message) {
+	o, err := s.owner.FindObject(mailbox)
+	if err != nil {
+		s.owner.core.LogErr(err)
+	}
+	if obj, ok := o.(Object); ok {
+		obj.UnLockObjSuccess(false)
+	}
 	return 0, nil
 }
