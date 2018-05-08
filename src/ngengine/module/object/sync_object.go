46814e76fe8004b4ee7de41aea4f4476c63e3dab
Peterosh <908789002@qq.com>
2018-05-08 16:10:57 +0800

    修改回掉函数参数

--- src/ngengine/module/object/sync_object.go
+++ src/ngengine/module/object/sync_object.go
@@ -25,94 +25,107 @@ func (s *SyncObject) RegisterCallback(svr rpc.Servicer) {
 }
 
 // 对象属性变动
-func (s *SyncObject) UpdateAttr(mailbox rpc.Mailbox, msg *protocol.Message) (errcode int32, reply *protocol.Message) {
+func (s *SyncObject) UpdateAttr(sender, _ rpc.Mailbox, msg *protocol.Message) (errcode int32, reply *protocol.Message) {
 	return 0, nil
 }
 
 // 对象tupele属性变动
-func (s *SyncObject) UpdateTuple(mailbox rpc.Mailbox, msg *protocol.Message) (errcode int32, reply *protocol.Message) {
+func (s *SyncObject) UpdateTuple(sender, _ rpc.Mailbox, msg *protocol.Message) (errcode int32, reply *protocol.Message) {
 	return 0, nil
 }
 
 // 对象表格增加一行
-func (s *SyncObject) AddTableRow(mailbox rpc.Mailbox, msg *protocol.Message) (errcode int32, reply *protocol.Message) {
+func (s *SyncObject) AddTableRow(sender, _ rpc.Mailbox, msg *protocol.Message) (errcode int32, reply *protocol.Message) {
 	return 0, nil
 }
 
 // 对象表格增加一行，并设置值
-func (s *SyncObject) AddTableRowValue(mailbox rpc.Mailbox, msg *protocol.Message) (errcode int32, reply *protocol.Message) {
+func (s *SyncObject) AddTableRowValue(sender, _ rpc.Mailbox, msg *protocol.Message) (errcode int32, reply *protocol.Message) {
 	return 0, nil
 }
 
 // 设置表格一行的值
-func (s *SyncObject) SetTableRowValue(mailbox rpc.Mailbox, msg *protocol.Message) (errcode int32, reply *protocol.Message) {
+func (s *SyncObject) SetTableRowValue(sender, _ rpc.Mailbox, msg *protocol.Message) (errcode int32, reply *protocol.Message) {
 	return 0, nil
 }
 
 // 对象表格删除一行
-func (s *SyncObject) DelTableRow(mailbox rpc.Mailbox, msg *protocol.Message) (errcode int32, reply *protocol.Message) {
+func (s *SyncObject) DelTableRow(sender, _ rpc.Mailbox, msg *protocol.Message) (errcode int32, reply *protocol.Message) {
 	return 0, nil
 }
 
 // 对象表格清除所有行
-func (s *SyncObject) ClearTable(mailbox rpc.Mailbox, msg *protocol.Message) (errcode int32, reply *protocol.Message) {
+func (s *SyncObject) ClearTable(sender, _ rpc.Mailbox, msg *protocol.Message) (errcode int32, reply *protocol.Message) {
 	return 0, nil
 }
 
 // 对象表格单元格更新
-func (s *SyncObject) ChangeTable(mailbox rpc.Mailbox, msg *protocol.Message) (errcode int32, reply *protocol.Message) {
+func (s *SyncObject) ChangeTable(sender, _ rpc.Mailbox, msg *protocol.Message) (errcode int32, reply *protocol.Message) {
 
 	return 0, nil
 }
 
 // LockObj 给对象上锁
-func (s *SyncObject) LockObj(mailbox rpc.Mailbox, msg *protocol.Message) (errcode int32, reply *protocol.Message) {
-	m := protocol.NewMessageReader(reply)
+func (s *SyncObject) LockObj(sender, _ rpc.Mailbox, msg *protocol.Message) (errcode int32, reply *protocol.Message) {
+	m := protocol.NewMessageReader(msg)
+	var objmailbox rpc.Mailbox
+	m.Read(&objmailbox)
+
 	r, _ := m.ReadUInt32()
-	o, err := s.owner.FindObject(mailbox)
+	o, err := s.owner.FindObject(objmailbox)
 	if err != nil {
 		s.owner.core.LogErr(err)
 	}
 
 	if obj, ok := o.(Object); ok {
-		obj.AddLocker(mailbox, r, true)
+		obj.AddLocker(sender, r, true)
 	}
 	return 0, nil
 }
 
 // UnLockObj 给对象解锁
-func (s *SyncObject) UnLockObj(mailbox rpc.Mailbox, msg *protocol.Message) (errcode int32, reply *protocol.Message) {
-	m := protocol.NewMessageReader(reply)
+func (s *SyncObject) UnLockObj(sender, _ rpc.Mailbox, msg *protocol.Message) (errcode int32, reply *protocol.Message) {
+	m := protocol.NewMessageReader(msg)
+	var objmailbox rpc.Mailbox
+	m.Read(&objmailbox)
+
 	r, _ := m.ReadUInt32()
-	o, err := s.owner.FindObject(mailbox)
+	o, err := s.owner.FindObject(objmailbox)
 	if err != nil {
 		s.owner.core.LogErr(err)
 	}
 	if obj, ok := o.(Object); ok {
-		obj.UnLockObj(mailbox, r, true)
+		obj.UnLockObj(sender, r, true)
 	}
 	return 0, nil
 }
 
 // 远程通知对象上锁成功
-func (s *SyncObject) LockObjSuccess(mailbox rpc.Mailbox, msg *protocol.Message) (errcode int32, reply *protocol.Message) {
-	m := protocol.NewMessageReader(reply)
+func (s *SyncObject) LockObjSuccess(sender, _ rpc.Mailbox, msg *protocol.Message) (errcode int32, reply *protocol.Message) {
+	m := protocol.NewMessageReader(msg)
+	var objmailbox rpc.Mailbox
+	m.Read(&objmailbox)
+
 	r, _ := m.ReadUInt32()
-	o, err := s.owner.FindObject(mailbox)
+	o, err := s.owner.FindObject(objmailbox)
 	if err != nil {
 		s.owner.core.LogErr(err)
 	}
 
 	if obj, ok := o.(Object); ok {
 		// 这里远程回复上锁成功所以上锁的就是本对象切这里算上本地的锁
-		obj.LockObjSuccess(*obj.Original(), r, false)
+		obj.LockObjSuccess(objmailbox, r, false)
 	}
 	return 0, nil
 }
 
 // 远程通知对象解锁成功
-func (s *SyncObject) UnLockObjSuccess(mailbox rpc.Mailbox, msg *protocol.Message) (errcode int32, reply *protocol.Message) {
-	o, err := s.owner.FindObject(mailbox)
+func (s *SyncObject) UnLockObjSuccess(sender, _ rpc.Mailbox, msg *protocol.Message) (errcode int32, reply *protocol.Message) {
+	m := protocol.NewMessageReader(msg)
+	var objmailbox rpc.Mailbox
+	m.Read(&objmailbox)
+
+	o, err := s.owner.FindObject(objmailbox)
 	if err != nil {
 		s.owner.core.LogErr(err)
 	}
