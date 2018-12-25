98677d56706c17c586e925d550dd5b152afb12e1
liangliang.shi <liangliang.shi@gmail.com>
2018-12-25 16:23:25 +0800

    update fsm

--- common/fsm/fsm.go
+++ common/fsm/fsm.go
@@ -5,40 +5,81 @@ import (
 )
 
 const (
-	STOP = "-"
+	STOP   = "-"
+	ETIMER = -1
 )
 
+type StateRegister interface {
+	AddHandle(event int, handle StateHandle)
+}
+
 type State interface {
+	Init(StateRegister)
 	Enter()
-	Handle(event int, param interface{}) string
+	OnHandle(event int, param interface{}) string
+	OnTimer() string
 	Exit()
 }
 
-type Default struct{}
-
-func (d *Default) Enter() {
-
+type Default struct {
 }
 
-func (d *Default) Handle(event int, param interface{}) string {
-	return STOP
+func (d *Default) Init(StateRegister)                           {}
+func (d *Default) Enter()                                       {}
+func (d *Default) OnHandle(event int, param interface{}) string { return STOP }
+func (d *Default) OnTimer() string                              { return "" }
+func (d *Default) Exit()                                        {}
+
+type StateHandle func(event int, param interface{}) string
+type StateWrapper struct {
+	s  State
+	sf map[int]StateHandle
 }
 
-func (d *Default) Exit() {
+func NewState(s State) *StateWrapper {
+	d := new(StateWrapper)
+	d.s = s
+	d.sf = make(map[int]StateHandle)
+	return d
+}
 
+func (d *StateWrapper) Init() {
+	d.s.Init(d)
+}
+
+func (d *StateWrapper) AddHandle(event int, handle StateHandle) {
+	d.sf[event] = handle
+}
+
+func (d *StateWrapper) Enter() {
+	d.s.Enter()
+}
+
+func (d *StateWrapper) Handle(event int, param interface{}) string {
+	if event == ETIMER {
+		return d.s.OnTimer()
+	}
+	if h, ok := d.sf[event]; ok {
+		return h(event, param)
+	}
+	return d.s.OnHandle(event, param)
+}
+
+func (d *StateWrapper) Exit() {
+	d.s.Exit()
 }
 
 // 有限状态机
 type FSM struct {
-	state    map[string]State
+	state    map[string]*StateWrapper
 	def      string
-	current  State
+	current  *StateWrapper
 	curstate string
 }
 
 func NewFSM() *FSM {
 	f := &FSM{}
-	f.state = make(map[string]State)
+	f.state = make(map[string]*StateWrapper)
 	return f
 }
 
@@ -47,8 +88,9 @@ func (f *FSM) Register(name string, state State) {
 	if _, dup := f.state[name]; dup {
 		panic("register state twice")
 	}
-
-	f.state[name] = state
+	s := NewState(state)
+	s.Init()
+	f.state[name] = s
 }
 
 // 启动状态机, state是初始状态
@@ -68,6 +110,11 @@ func (f *FSM) Start(state string) error {
 	f.current = s
 	f.current.Enter()
 	return nil
+}
+
+// 超时
+func (f *FSM) Timeout() (bool, error) {
+	return f.Dispatch(ETIMER, nil)
 }
 
 // 派发事件
